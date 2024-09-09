10ece0f006c7cd6f4d65f7d46bb021526a75f2f9
Tan Phat <nguyentanphat03122004@gmail.com>
2024-09-09 18:17:27 +0700

    update

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@
 // Cập nhật sinh viên
 // Clear dữ liệu đang có trong form
 // Render dữ liệu lên trên giao diện (done)
-// Lưu trữ dữ liệu ở local storage
+// Lưu trữ dữ liệu ở local storage (done)
 // Validation dữ liệu
 // Tìm kiếm sinh viên
 // Thông báo cho người dùng
@@ -15,19 +15,11 @@ let arrSinhVien = [];
 document.getElementById("formQLSV").onsubmit = function (event) {
   //prevendDefault dùng để ngăn chặn sự kiện reload
   event.preventDefault();
-
   // Thực hiện xử lý và truy cập lấy tất cả dữ liệu từ các input có trong giao diện
-  let arrField = document.querySelectorAll("#formQLSV input,select");
-  let sinhVien = new SinhVien();
-  for (let field of arrField) {
-    // destructuring
-    let { value, id } = field;
-    sinhVien[id] = value;
-  }
+  let sinhVien = getValueForm();
   arrSinhVien.push(sinhVien);
-  setLocalStorage("sinhVien", arrSinhVien);
+  setLocalStorage("arrSinhVien", arrSinhVien);
   renderDataSinhVien();
-
   // trỏ tới thẻ form đang chạy unsubmit
   // xóa các giá trị người dùng nhập sau khi submit (reset lại các ô input)
   event.target.reset();
@@ -41,7 +33,6 @@ function renderDataSinhVien(arr = arrSinhVien) {
     // tạo ra một đối tượng mới ===> có phương thức nhưng không có dữ liệu
     let newSinhVien = new SinhVien();
     Object.assign(newSinhVien, sinhVien);
-
     let { txtMaSV, txtTenSV, txtEmail, txtNgaySinh, khSV } = newSinhVien;
     content += `
             <tr>
@@ -52,8 +43,8 @@ function renderDataSinhVien(arr = arrSinhVien) {
                 <td>${khSV}</td>
                 <td>${newSinhVien.tinhDiemTrungBinh().toFixed(2)}</td>
                 <td>
-                    <button class="btn btn-danger">Xóa</button>
-                    <button class="btn btn-warning">Sửa</button>
+                    <button onclick="deleteSinhVien('${txtMaSV}')" class="btn btn-danger">Xóa</button>
+                    <button onclick="getInfoSinhVien('${txtMaSV}')" class="btn btn-warning">Sửa</button>
                 </td>
             </tr>
         `;
@@ -62,8 +53,9 @@ function renderDataSinhVien(arr = arrSinhVien) {
 }
 
 window.onload = function () {
-  let dataLocal = getLocalStorage("arrSinhVien"); // data | null
+  let dataLocal = getLocalStorage("arrSinhVien");
   if (dataLocal) {
+    console.log(dataLocal);
     arrSinhVien = dataLocal;
     renderDataSinhVien();
   }
@@ -73,15 +65,15 @@ window.onload = function () {
 // 3 phương thức chính : setItem , getItem , removeItem
 
 // setItem() truyền vào 2 giá trị là key (tên dữ liệu cần lưu), giá trị của key
-localStorage.setItem("hoTen", "Tấn Phát");
-let arrMonAn = [
-  {
-    tenMon: "Màn thầu",
-  },
-  {
-    tenMon: "Xí Quách",
-  },
-];
+// localStorage.setItem("hoTen", "Tấn Phát");
+// let arrMonAn = [
+//   {
+//     tenMon: "Màn thầu",
+//   },
+//   {
+//     tenMon: "Xí Quách",
+//   },
+// ];
 // biến đổi những object trong mảng thành chuỗi bằng JSON.stringify()
 // let dataString = JSON.stringify(arrMonAn);
 // console.log(dataString);
@@ -103,4 +95,71 @@ function setLocalStorage(key, data) {
 function getLocalStorage(key) {
   let dataLocal = localStorage.getItem(key);
   return dataLocal ? JSON.parse(dataLocal) : null;
+}
+// ----------- CHỨC NĂNG XÓA SINH VIÊN --------------
+function deleteSinhVien(maSV) {
+  // khi xóa 1 phần tử, sử dụng phương thức splice(vị trí bắt đầu, số lượng phần tử cần xóa)
+  // xử lý tìm kiếm vị trí sinh viên trong mảng
+  let index = arrSinhVien.findIndex((item, i) => item.txtMaSV == maSV);
+  if (index != -1) {
+    arrSinhVien.splice(index, 1);
+    renderDataSinhVien();
+    setLocalStorage("arrSinhVien", arrSinhVien);
+  }
+  // thao tác xóa sinh viên cập nhật dữ liệu
+}
+
+//---------get info sinhvien -------------
+// B1. gắn function getinfoSinhVien vao nút sửa
+// B2. thực hiện tìm kiếm sinh viên trong mảng
+// B3. thực hiện đưa dữ liệu viết lên input trong form cho người dùng chỉnh sửa
+// B4. Ngăn chặn người dùng chỉnh sửa mã sinh viên (disabled, read only)
+function getInfoSinhVien(maSV) {
+  let sinhVien = arrSinhVien.find((item, index) => item.txtMaSV == maSV);
+  if (sinhVien) {
+    let arrField = document.querySelectorAll(
+      "#formQLSV input,#formQLSV select"
+    );
+    for (let field of arrField) {
+      // field đại diện cho các select input tìm kiếm được trong form
+      field.value = sinhVien[field.id];
+      if (field.id == "txtMaSV") {
+        field.readOnly = true;
+      }
+    }
+  }
+}
+
+// chức năng cập nhật sinh viên
+// B1. DOM tới nút button cập nhật và tạo một sự kiện click
+// B2. Xử lý lấy dữ liệu người dùng đã cập nhật trên form
+// B3. Tìm kiếm tới vị trí của phần tử được cập nhật
+// B4. Thay thế dữ liệu mới vào vị trí của phần tử được cập nhật
+// B5. Thực hiện chạy lại hàm render và cập nhật xuống local storage
+// B6. CLear toàn bộ dữ liệu của form và tắt readOnly của input
+
+document.getElementById("updateSV").onclick = function () {
+  let sinhVien = getValueForm();
+  let index = arrSinhVien.findIndex(
+    (item, i) => item.txtMaSV == sinhVien.txtMaSV
+  );
+  if (index != -1) {
+    arrSinhVien[index] = sinhVien;
+    renderDataSinhVien();
+    setLocalStorage("arrSinhVien", arrSinhVien);
+    document.getElementById("txtMaSV").readOnly = false;
+    document.getElementById("formQLSV").reset();
+  }
+};
+
+//----------- get value form ----------------
+function getValueForm() {
+  let arrField = document.querySelectorAll("#formQLSV input,#formQLSV select");
+  let sinhVien = new SinhVien();
+  for (let field of arrField) {
+    // destructuring
+    let { value, id } = field;
+    sinhVien[id] = value;
+  }
+  return sinhVien;
 }
